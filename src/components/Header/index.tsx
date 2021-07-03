27a476c8a60c397b931545b9071b3b647f26547f
pedro122723 <correia.pedro@outlook.com>
2021-07-03 01:53:31 -0300

    a

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import * as Styled from './styles'
+
+const Header: React.FC = () => {
+    return (
+        <Styled.Header>
+            <div>
+                <img src="" />
+            </div>
+        </Styled.Header>
+    )
+}
+
+export default Header;
